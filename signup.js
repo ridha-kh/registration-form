79ba974bf32f53dc85db384e7251059a46cbaa25
ridha-kh <ridakh43@gmail.com>
2026-06-21 23:34:13 +0100

    add api to the login and signup pages

--- signup.js
+++ signup.js
@@ -6,3 +6,38 @@ document.addEventListener("DOMContentLoaded", function () {
         signupForm.style.opacity = 1;
     }, 500);
 }); 
+
+
+document.getElementById("signupForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector("input[name='username']").value;
+    const email = document.querySelector("input[name='email']").value;
+    const password = document.querySelector("input[name='password']").value;
+
+    try {
+        const response = await fetch("http://localhost:8080/api/student/save", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password
+            })
+        });
+
+        if (response.ok) {
+            alert("Student registered successfully!");
+            window.location.href = "login.html";
+        } else {
+            const error = await response.text();
+            alert("Error: " + error);
+        }
+
+    } catch (err) {
+        console.error(err);
+        alert("Cannot connect to server");
+    }
+});
